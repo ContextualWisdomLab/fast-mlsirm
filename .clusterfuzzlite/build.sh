ee54b9dbbe31e56ba0cbc360e0c9646b45b58c47
Seongho Bae <seongho@kw.ac.kr>
2026-07-10 15:56:59 +0900

    fix: allow non-root fuzz builds

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -eux
 
 cd "$SRC/fast-mlsirm"
+mkdir -p "$OUT"
+id
+ls -ld "$SRC/fast-mlsirm" "$OUT" "$(dirname "$LIB_FUZZING_ENGINE")"
 cargo fuzz build -O neg_loglik
 cp fuzz/target/x86_64-unknown-linux-gnu/release/neg_loglik "$OUT/"
