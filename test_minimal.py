4cc6cd8ce99bcdc0fdad6a9622f93d4ebe4e99ff
seonghobae <8172694+seonghobae@users.noreply.github.com>
2026-07-29 12:46:00 +0000

    Clean up temporary files and keep 3D distance optimization

--- test_minimal.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-import fast_mlsirm.fitstats
-print("Import successful")
--- update_marginal.sh
+++
@@ -1,2 +0,0 @@
-# Check where dist is calculated using 3D broadcasting in python/fast_mlsirm/estimators/marginal.py
-grep -n "eps_distance + np.sum(diff \* diff" python/fast_mlsirm/estimators/marginal.py
