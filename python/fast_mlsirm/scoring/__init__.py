fc6379302827441e912871c93bca65ad756f206e
Seongho Bae <me@seonghobae.me>
2026-08-04 10:29:15 +0900

    feat(scoring): export observation contracts

--- python/fast_mlsirm/scoring/__init__.py
+++ python/fast_mlsirm/scoring/__init__.py
@@ -19,12 +19,25 @@ from .contracts import ValidationPolicy as ValidationPolicy
 from .contracts import artifact_digest as artifact_digest
 from .contracts import build_assessment_spec as build_assessment_spec
 from .contracts import canonical_json as canonical_json
+from .observations import MAX_EVIDENCE_OFFSET as MAX_EVIDENCE_OFFSET
+from .observations import MAX_EVIDENCE_SPANS as MAX_EVIDENCE_SPANS
+from .observations import MAX_OBSERVATIONS as MAX_OBSERVATIONS
+from .observations import EvidenceSpan as EvidenceSpan
+from .observations import ObservationLevel as ObservationLevel
+from .observations import ObservationStatus as ObservationStatus
+from .observations import RaterKind as RaterKind
+from .observations import ScoreObservation as ScoreObservation
+from .observations import build_score_observation as build_score_observation
+from .observations import validate_observations as validate_observations
 
 __all__ = [
     "ASSESSMENT_SCHEMA_VERSION",
+    "MAX_EVIDENCE_OFFSET",
+    "MAX_EVIDENCE_SPANS",
     "MAX_METADATA_COLLECTION_VALUES",
     "MAX_METADATA_DEPTH",
     "MAX_METADATA_NODES",
+    "MAX_OBSERVATIONS",
     "AdjudicationPolicy",
     "AssessmentResponseType",
     "AssessmentSpec",
@@ -32,10 +45,17 @@ __all__ = [
     "CalibrationPolicy",
     "ConstructSpec",
     "EnginePolicy",
+    "EvidenceSpan",
     "MonitoringPolicy",
+    "ObservationLevel",
+    "ObservationStatus",
+    "RaterKind",
     "ReportingPolicy",
+    "ScoreObservation",
     "ValidationPolicy",
     "artifact_digest",
     "build_assessment_spec",
+    "build_score_observation",
     "canonical_json",
+    "validate_observations",
 ]
