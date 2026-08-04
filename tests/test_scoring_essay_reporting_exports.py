15b80c47003268c6141fc40000ae2a88097ac4fd
Seongho Bae <me@seonghobae.me>
2026-08-05 01:48:51 +0900

    test(essay): verify standalone report export

--- tests/test_scoring_essay_reporting_exports.py
+++ tests/test_scoring_essay_reporting_exports.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import fast_mlsirm.scoring.essay as essay
+import fast_mlsirm.scoring.essay.report_html as report_html
 import fast_mlsirm.scoring.essay.reporting as reporting
 
 
@@ -12,6 +13,7 @@ def test_score_reporting_symbols_are_exported_from_essay_package() -> None:
         "EssayScoreReport",
         "MAX_ESSAY_REPORT_REVIEW_TRIGGERS",
         "build_essay_score_report",
+        "render_essay_score_report_html",
     }
 
     assert expected <= set(essay.__all__)
@@ -21,5 +23,10 @@ def test_score_reporting_symbols_are_exported_from_essay_package() -> None:
         == reporting.MAX_ESSAY_REPORT_REVIEW_TRIGGERS
     )
     assert essay.build_essay_score_report is reporting.build_essay_score_report
+    assert (
+        essay.render_essay_score_report_html
+        is report_html.render_essay_score_report_html
+    )
     assert essay.EssayScoreReport.__doc__
     assert essay.build_essay_score_report.__doc__
+    assert essay.render_essay_score_report_html.__doc__
