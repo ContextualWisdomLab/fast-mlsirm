b1e906c17cde20a95e1cc86d48601f412b54790b
Seongho Bae <me@seonghobae.me>
2026-08-23 03:36:30 -0700

    fix(irt): install polytomous prediction resource guard

--- python/fast_mlsirm/__init__.py
+++ python/fast_mlsirm/__init__.py
@@ -6,11 +6,13 @@ from importlib.metadata import PackageNotFoundError as _PackageNotFoundError
 from importlib.metadata import version as _distribution_version
 
 from . import _legacy_init as _legacy_init
+from . import polytomous as _polytomous
 from . import reliability as _reliability
 from . import scaling as _scaling
 from . import validation as _validation
 from ._fleiss_control_safety import install as _install_fleiss_control_safety
 from ._icc_control_safety import install as _install_icc_control_safety
+from ._polytomous_prediction_safety import install as _install_polytomous_prediction_safety
 from ._scaling_control_safety import install as _install_scaling_control_safety
 
 # Harden historical public adapters before copying legacy exports. These
@@ -19,6 +21,7 @@ from ._scaling_control_safety import install as _install_scaling_control_safety
 _install_icc_control_safety(_reliability)
 _install_scaling_control_safety(_scaling)
 _install_fleiss_control_safety(_validation)
+_install_polytomous_prediction_safety(_polytomous)
 _legacy_init.icc = _reliability.icc
 _legacy_init.bradley_terry_mm = _scaling.bradley_terry_mm
 _legacy_init.fleiss_kappa = _validation.fleiss_kappa
@@ -26,7 +29,9 @@ _legacy_init.fleiss_kappa = _validation.fleiss_kappa
 del (
     _install_fleiss_control_safety,
     _install_icc_control_safety,
+    _install_polytomous_prediction_safety,
     _install_scaling_control_safety,
+    _polytomous,
     _reliability,
     _scaling,
 )
